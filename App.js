3f414ca11a0e4650e5a696b90a7b2ea1e683277f
alex.wong <wonghoming111105@gmail.com>
2021-04-13 02:40:13 +0800

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import {
+  Button,
+  Flex,
+  WhiteSpace,
+  WingBlank,
+  Card,
+} from "@ant-design/react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.logo}>
+        <Image
+          style={styles.image}
+          source={{
+            uri: "https://reactnative.dev/img/tiny_logo.png",
+          }}
+        />
+        <Text style={{marginLeft:20}}>Note.js</Text>
+      </View>
+
+      <View>
+        <WingBlank size="lg">
+          <Card>
+            <Card.Header
+              title="This is title"
+              thumbStyle={{ width: 30, height: 30 }}
+              thumb="https://gw.alipayobjects.com/zos/rmsportal/MRhHctKOineMbKAZslML.jpg"
+              extra="this is extra"
+            />
+            <Card.Body>
+              <View style={{ height: 42 }}>
+                <Text style={{ marginLeft: 16 }}>Card Content</Text>
+              </View>
+            </Card.Body>
+            <Card.Footer
+              content="footer content"
+              extra="footer extra content"
+            />
+          </Card>
+        </WingBlank>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +52,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    // alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    height: 40,
+    width: 40,
+  },
+  logo: {
+    alignItems: "center",
+    flexDirection: "row",
+    alignSelf: "center",
   },
 });
